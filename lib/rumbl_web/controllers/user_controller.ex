62f0b9ff56ac9ea9f1d398837a0df91c172286e6
Ralph Bankston <ralph.l.bankston@gmail.com>
2018-10-07 12:49:01 -0400

    Adding logged in pages

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -5,9 +5,15 @@ defmodule RumblWeb.UserController do
     alias Rumbl.Accounts.User
 
     def index(conn, _params) do
-        users = Accounts.list_users()
-        render(conn, "index.html", users: users)
-      end
+        case authenticate(conn) do
+            %Plug.Conn{halted: true} = conn ->
+                conn
+
+            conn ->
+                users = Accounts.list_users()
+                render(conn, "index.html", users: users)
+        end
+    end
   
     def show(conn, %{"id" => id}) do
         user = Accounts.get_user(id)
@@ -27,6 +33,17 @@ defmodule RumblWeb.UserController do
                 |> redirect(to: Routes.user_path(conn, :index))
             {:error, %Ecto.Changeset{} = changeset} ->
                 render(conn, "new.html", changeset: changeset)
+        end
+    end
+
+    defp authenticate(conn) do
+        if conn.assigns.current_user do
+            conn
+        else
+            conn
+            |> put_flash(:error, "You must be logged in to access that page")
+            |> redirect(to: Routes.page_path(conn, :index))
+            |> halt()
         end
     end
     
