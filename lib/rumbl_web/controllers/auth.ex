d6cacecf4cc73dedb6396becbf9b557a78f9847c
Ralph Bankston <ralph.l.bankston@gmail.com>
2018-10-07 16:04:24 -0400

    Generated videos in the manage scope

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -1,6 +1,8 @@
 defmodule RumblWeb.Auth do
     import Plug.Conn
+    import Phoenix.Controller
 
+    alias RumblWeb.Router.Helpers, as: Routes
     alias Rumbl.Accounts
 
     def init(opts), do: opts
@@ -28,5 +30,16 @@ defmodule RumblWeb.Auth do
 
     def logout(conn) do
         configure_session(conn, drop: true)
+    end
+
+    def authenticate_user(conn, _opts) do
+        if conn.assigns.current_user do
+            conn
+        else
+            conn
+            |> put_flash(:error, "You must be logged in to access that page")
+            |> redirect(to: Routes.page_path(conn, :index))
+            |> halt()
+        end
     end
 end
